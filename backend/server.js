3aa91af4117bdc03e9cfc3e6de4ccda8c167a37f
Claudia Nicolae <nicolaeclaudia19@stud.ase.ro>
2023-01-21 11:14:18 +0200

    added in crud ops for entities

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,10 @@ const Painting = sequelize.define(
       type: Sequelize.STRING,
       allowNull: false,
     },
+    imageUrl: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
     artistId: {
       type: Sequelize.INTEGER,
       references: {
@@ -64,6 +68,34 @@ const Painting = sequelize.define(
 
 Artist.hasMany(Painting, { foreignKey: "artistId" });
 Painting.belongsTo(Artist, { foreignKey: "artistId" });
+
+async function createArtist(artistName, artistDOB) {
+  try {
+    const newArtist = Artist.build({
+      artistName: artistName,
+      artistDOB: artistDOB,
+    });
+    await newArtist.save();
+    console.log("Artist created successfully: ", newArtist);
+  } catch (error) {
+    console.error("Error creating artist: ", error);
+  }
+}
+
+async function createPainting(paintingId, title, imageUrl, artistId) {
+  try {
+    const newPainting = Painting.build({
+      paintingId: paintingId,
+      title: title,
+      imageUrl: imageUrl,
+      artistId: artistId,
+    });
+    await newPainting.save();
+    console.log("Painting created successfully: ", newPainting);
+  } catch (error) {
+    console.error("Error creating artist: ", error);
+  }
+}
 
 sequelize
   .authenticate()
@@ -83,36 +115,12 @@ sequelize
     console.error("Error syncing models: ", error);
   });
 
-export async function createArtist(artistName, artistDOB) {
-  try {
-    const newArtist = Artist.build({
-      artistName: artistName,
-      artistDOB: artistDOB,
-    });
-    await newArtist.save();
-    console.log("Artist created successfully: ", newArtist);
-  } catch (error) {
-    console.error("Error creating artist: ", error);
-  }
-}
-
-export async function createPainting(paintingId, title, artistId) {
-  try {
-    const newPainting = Painting.build({
-      paintingId: paintingId,
-      title: title,
-      artistId: artistId,
-    });
-    await newPainting.save();
-    console.log("Painting created successfully: ", newPainting);
-  } catch (error) {
-    console.error("Error creating artist: ", error);
-  }
-}
-
 app.use(bodyParser.json());
 
+//============Artist CRUD ops============
+
 // Get all artists
+
 app.get("/artists", async (req, res) => {
   try {
     const artists = await Artist.findAll();
@@ -123,6 +131,7 @@ app.get("/artists", async (req, res) => {
 });
 
 // Get artist by ID
+
 app.get("/artists/:id", async (req, res) => {
   try {
     const artist = await Artist.findByPk(req.params.id);
@@ -137,6 +146,7 @@ app.get("/artists/:id", async (req, res) => {
 });
 
 // Create a new artist
+
 app.post("/artists", async (req, res) => {
   try {
     const newArtist = await createArtist(
@@ -150,6 +160,7 @@ app.post("/artists", async (req, res) => {
 });
 
 // Update an artist
+
 app.put("/artists/:id", async (req, res) => {
   try {
     const artist = await Artist.findByPk(req.params.id);
@@ -165,6 +176,7 @@ app.put("/artists/:id", async (req, res) => {
 });
 
 // Delete an artist
+
 app.delete("/artists/:id", async (req, res) => {
   try {
     const artist = await Artist.findByPk(req.params.id);
@@ -173,6 +185,96 @@ app.delete("/artists/:id", async (req, res) => {
     } else {
       await artist.destroy();
       res.json({ message: "Artist deleted successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//============Painting CRUD ops============
+
+// Get all paintings
+
+app.get("/paintings", async (req, res) => {
+  try {
+    const paintings = await Painting.findAll({
+      include: [
+        {
+          model: Artist,
+          attributes: ["artistName"],
+        },
+      ],
+    });
+    res.json(paintings);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get painting by ID
+
+app.get("/paintings/:id", async (req, res) => {
+  try {
+    const painting = await Painting.findByPk(req.params.id, {
+      include: [
+        {
+          model: Artist,
+          attributes: ["artistId"],
+        },
+      ],
+    });
+    if (!painting) {
+      res.status(404).json({ message: "Painting not found" });
+    } else {
+      res.json(painting);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Create a new painting
+
+app.post("/paintings", async (req, res) => {
+  try {
+    const newPainting = await Painting.create({
+      paintingId: req.body.paintingId,
+      title: req.body.title,
+      imageUrl: req.body.imageUrl,
+      artistId: req.body.artistId,
+    });
+    res.status(201).json(newPainting);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Update a painting
+
+app.put("/paintings/:id", async (req, res) => {
+  try {
+    const painting = await Painting.findByPk(req.params.id);
+    if (!painting) {
+      res.status(404).json({ message: "Painting not found" });
+    } else {
+      await painting.update(req.body);
+      res.json(painting);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Delete a painting
+
+app.delete("/paintings/:id", async (req, res) => {
+  try {
+    const painting = await Painting.findByPk(req.params.id);
+    if (!painting) {
+      res.status(404).json({ message: "Painting not found" });
+    } else {
+      await painting.destroy();
+      res.json({ message: "Painting deleted successfully" });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
